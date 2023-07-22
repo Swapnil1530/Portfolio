ad3d045578c4fc52fc95d597ce018b1cab24a8b3
Swapnil1530 <patelswapnil1004@gmail.com>
2023-07-22 16:36:55 +0530

    initial commit ✨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import Footer from '../components/Footer';
+import SocialSidebar from '../components/SocialSidebar';
+
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div>
+
+        <Header />
+          <SocialSidebar />
+          <div className="max-w-[1430px] mx-auto  md:px-36">
+              <div className="max-w-[1140px] xl:border-light-gray xl:border-r xl:border-l dark:border-gray-800">
+                  <main className="max-w-[1000px] mx-auto relative z-20">
+          {children}
+                      <Footer />
+                  </main>
+              </div>
+          </div>
+
+      </div>
+      </body>
     </html>
   )
 }
