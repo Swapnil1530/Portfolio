36edd249f7b543f8b206519da2778b505540a5e8
Swapnil1530 <patelswapnil1004@gmail.com>
2023-08-02 22:05:06 +0530

    feat : in header make mobile responsive ✨

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 
-const linkData: any = [
+const linkData = [
   {
     lable: "Home",
     path: "/",
@@ -35,27 +35,69 @@ const linkData: any = [
     targetSegment: "Contact",
   },
 ];
+
 const Header = () => {
   const activeSegment = useSelectedLayoutSegment();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      <nav className="p-4 flex items-center justify-center bg-[#181818] fixed top-0 z-40 w-full max-w-[1905px] transform right-1/2 translate-x-1/2   transition duration-500  false">
-        <ul className="flex gap-16 ">
-          {linkData.map((link: any, index: any) => {
-            return (
+      <nav className="p-4 flex  bg-[#181818] fixed top-0 z-40 w-full transform  transition duration-500 false lg:hidden">
+        <div className="flex-row ">
+          <button
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            aria-label="menu"
+            className="flex lg:hidden flex-col w-8 h-8 space-y-[6px] justify-center"
+          >
+            {!isMenuOpen ? (
+              <>
+              <span className="w-full h-[2px] bg-[#f72b1c] transform origin-top-right transition duration-500"></span>
+              <span className="w-full h-[2px] bg-white -ml-2 transform transition duration-500"></span>
+              <span className="w-full h-[2px] bg-[#f72b1c] transform origin-bottom-right transition duration-500"></span>
+              </>
+            ):(
+              <>
+              <span className="w-full h-[2px] bg-[#f72b1c] transform origin-center rotate-45 transition duration-500"></span>
+              <span className="w-full h-[2px] bg-white transform origin-center -rotate-45 transition duration-500"></span>
+              </>
+            )}
+            
+          </button>
+        </div>
+        {isMenuOpen && (
+          <ul className="flex flex-col gap-4 mt-4 items-center justify-center w-full">
+            {linkData.map((link, index) => (
               <Link
                 key={index}
                 href={link.path}
                 className={`${
                   activeSegment === link.targetSegment
-                    ? " text-[#f72b1c] "
-                    : "text-sm text-center transition block py-3 lg:py-0 rounded-lg lg:bg-transparent  text-white tracking-widest font-bold hover:text-[#f72b1c] "
+                    ? "text-[#f72b1c] text-center"
+                    : "text-sm text-center transition block py-3 lg:py-0 rounded-lg lg:bg-transparent text-white tracking-widest font-bold hover:text-[#f72b1c]"
                 }`}
               >
                 {link.lable}
               </Link>
-            );
-          })}
+            ))}
+          </ul>
+        )}
+      </nav>
+
+      <nav className="hidden lg:flex p-4 items-center justify-center bg-[#181818] fixed top-0 z-40 w-full max-w-[1905px] transform right-1/2 translate-x-1/2 transition duration-500 false">
+        <ul className="flex gap-16">
+          {linkData.map((link, index) => (
+            <Link
+              key={index}
+              href={link.path}
+              className={`${
+                activeSegment === link.targetSegment
+                  ? "text-[#f72b1c]"
+                  : "text-sm text-center transition block py-3 lg:py-0 rounded-lg lg:bg-transparent text-white tracking-widest font-bold hover:text-[#f72b1c]"
+              }`}
+            >
+              {link.lable}
+            </Link>
+          ))}
         </ul>
       </nav>
     </>
